bc306b7a1390246a2107bdbe64d91db48f9568cc
pengfei.miao <pengfei.miao@thoughtworks.com>
2023-04-24 21:17:20 +0800

    refactor: user next/router to bind MenuItem instead of Link

--- src/components/CollapseNavbar.tsx
+++ src/components/CollapseNavbar.tsx
@@ -2,38 +2,35 @@ import React, { useState } from 'react';
 import {
   Box,
   Flex,
-  Text,
   HStack,
-  VStack,
-  Divider,
-  Spacer,
-  Skeleton,
   Button,
   Collapse,
-  Center,
   IconButton,
-  Icon,
-  Grid,
-  AvatarGroup,
-  GridItem,
-  Avatar,
   Heading,
-  SystemProps,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   useColorModeValue,
+  Text,
 } from '@chakra-ui/react';
 import { siteConfig } from '@/../../config/site.config';
 import ThemeToggle from '@/components/ThemeToggle';
 import { ChevronDownIcon, MinusIcon } from '@chakra-ui/icons';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const CollapseNavbar = () => {
+  const router = useRouter();
+
   const [isOpen, setIsOpen] = useState(true);
 
   const borderColor = useColorModeValue('gray.300', 'gray.400');
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, url: string) => {
+    e.preventDefault();
+    router.push(url);
+  };
 
   return (
     <Box
@@ -64,17 +61,17 @@ const CollapseNavbar = () => {
                 Pages
               </MenuButton>
               <MenuList>
-                <MenuItem>
-                  <Link href="/">Dashboard</Link>
+                <MenuItem onClick={(e) => handleClick(e, '/')}>
+                  <Text>Dashboard</Text>
                 </MenuItem>
-                <MenuItem>
-                  <Link href="/example/build-status-overview">Build Status</Link>
+                <MenuItem onClick={(e) => handleClick(e, '/example/build-status-overview')}>
+                  <Text>Build Status</Text>
                 </MenuItem>
-                <MenuItem>
-                  <Link href="/example/ticket-status-overview">Ticket Status</Link>
+                <MenuItem onClick={(e) => handleClick(e, '/example/ticket-status-overview')}>
+                  <Text>Ticket Status</Text>
                 </MenuItem>
-                <MenuItem>
-                  <Link href="/example/project-timeline">Project Timeline</Link>
+                <MenuItem onClick={(e) => handleClick(e, '/example/project-timeline')}>
+                  <Text>Project Timeline</Text>
                 </MenuItem>
               </MenuList>
             </Menu>
