1f986d831f812e1f2b36601412350b4b7b3bd6b9
Siyuan Li <siyuan.li2@thoughtworks.com>
2023-04-28 09:53:15 +0800

    refine: refine owner_rotation.config.js

--- config/owner_rotation.config.js
+++ config/owner_rotation.config.js
@@ -1,39 +1,68 @@
 export const ownerRotationConfig = {
   title: 'Owner Rotation',
-  // datasource can be ApiTable or GoogleSheet, which means different source of datasheet
-  datasource: 'ApiTable',
-  // the base url of datasource, which is different between ApiTable and GoogleSheet
-  // baseUrl: 'https://docs.google.com/spreadsheets/d/',
-  baseUrl: 'https://apitable.com/fusion/v1/datasheets/',
   // refresh interval for Owner Rotation, when it's zero it means close the auto refresh
   refreshIntervalSeconds: 0,
-  // key - which represent api token in ApiTable or docId in GoogleSheet.
-  // for ApiTable you can see the api token in user profile settings,
-  // for GoogleSheet you can find the docId following the baseUrl of GoogleSheet link.
-  key: 'uskM0EJKuuJcPQu6waAF0Or',
-  // datasheets you want to monitor
-  rotations: [
-    {
-      subject: 'Stand Up',
-      color: 'green',
-      // for ApiTable it means apiTableId in the url
-      // for GoogleSheet it means sheetName below the document
-      sheetId: 'sheet-id1',
+  // datasource can be localData or ApiTable or GoogleSheet, which means different source of datasheet
+  datasource: {
+    // use local static data, could be masked in process.env.YOUR_KEY
+    localData: {
+      enabled: false,
+      rotations: [
+        {
+          subject: 'Stand Up',
+          color: 'green',
+          members: [
+            {
+              name: 'Tom',
+              startDate: '2023-01-01',
+              endDate: '2024-01-01',
+            },
+            {
+              name: 'Jerry',
+              startDate: '2024-01-01',
+              endDate: '2025-01-01',
+            },
+          ],
+        },
+      ],
     },
-    {
-      subject: 'BAU',
-      color: 'blue',
-      sheetId: 'sheet-id2',
+    // use API Table, register here https://apitable.com/
+    apiTable: {
+      enabled: false,
+      // for ApiTable you can see the api token in user profile settings,
+      apiKey: process.env.APITABLE_API_KEY,
+      baseUrl: 'https://apitable.com/fusion/v1/datasheets/',
+      rotations: [
+        {
+          subject: 'Stand Up',
+          color: 'green',
+          viewId: 'view-id1',
+        },
+        {
+          subject: 'Showcase',
+          color: 'blue',
+          viewId: 'view-id2',
+        },
+      ],
     },
-    {
-      subject: 'Retro',
-      color: 'purple',
-      sheetId: 'sheet-id3',
+    // use Google Sheet, the doc must be public
+    googleSheet: {
+      enabled: false,
+      baseUrl: 'https://docs.google.com/spreadsheets/d/',
+      // for GoogleSheet you can find the docId following the baseUrl of GoogleSheet link.
+      docId: '15txMkkkWBgxS7PCpInC3NBg9kaEi-ZhHjdALNRV-5G8',
+      rotations: [
+        {
+          subject: 'Stand Up',
+          color: 'green',
+          sheetName: 'sheet-name1',
+        },
+        {
+          subject: 'Showcase',
+          color: 'blue',
+          sheetName: 'sheet-name2',
+        },
+      ],
     },
-    {
-      subject: 'Showcase',
-      color: 'teal',
-      sheetId: 'sheet-id4',
-    },
-  ],
+  },
 };
