fc0ed58edd1011a56cbebb70016572bcdff50c8b
Botao Shen <shenbotao@live.cn>
2023-04-28 23:37:14 +0800

    feat: add api handler for GitHub actions.

---
+++ src/pages/api/github_build_status.ts
@@ -0,0 +1,105 @@
+import { NextApiHandler } from 'next';
+import { buildStatusConfig } from '@/../config/build_status.config';
+import { getBuildStatusFakeData } from '@/../../fake/build_status.fake';
+import { delay1s } from '@/lib/delay';
+
+interface workflowRunResponse {
+  total_count: number;
+  workflow_runs: WorkflowRun[];
+}
+
+/**
+ * An invocation of a workflow
+ */
+interface WorkflowRun {
+  id: number;
+  name?: string | null;
+  head_branch: string | null;
+  head_sha: string;
+  path: string;
+  run_number: number;
+  status: string | null;
+  conclusion: string | null;
+  workflow_id: number;
+  created_at: string;
+  updated_at: string;
+  actor?: SimpleUser;
+  triggering_actor?: SimpleUser;
+  run_started_at?: string;
+  jobs_url: string;
+  head_commit: null | SimpleCommit;
+  display_title: string;
+}
+
+interface SimpleUser {
+  login: string;
+  avatar_url: string;
+  gravatar_id: string | null;
+}
+/**
+ * A commit.
+ */
+interface SimpleCommit {
+  id: string;
+  tree_id: string;
+  message: string;
+  timestamp: string;
+  author: {
+    name: string;
+    email: string;
+  } | null;
+  committer: {
+    name: string;
+    email: string;
+  } | null;
+}
+
+const githubActionsConfig = buildStatusConfig.datasource.github;
+
+const handler: NextApiHandler = async (req, res) => {
+  getAllStatus()
+    .then((response) => res.status(200).json(response))
+    .catch((err) => res.status(500).send(err.message));
+};
+
+const getAllStatus = async () => {
+  if (!githubActionsConfig.apiToken) {
+    return delay1s(getBuildStatusFakeData);
+  }
+  return await Promise.all(githubActionsConfig.projects.map((project) => getStatus(project)));
+};
+
+const getStatus = async ({
+  owner,
+  repo,
+  branch,
+  workflowId,
+}: {
+  owner: string;
+  repo: string;
+  branch: string;
+  workflowId: number;
+}) => {
+  const url = `${githubActionsConfig.baseUrl}/repos/${owner}/${repo}/actions/workflows/${workflowId}/runs?per_page=1&branch=${branch}`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${githubActionsConfig.apiToken}`,
+    },
+  });
+  let json: workflowRunResponse = await response.json();
+  if (!response.ok) {
+    throw new Error(JSON.stringify(json));
+  }
+  const workflowRun = json.workflow_runs[0];
+  return {
+    projectName: repo,
+    branch: branch,
+    status: workflowRun.conclusion,
+    stopTime: workflowRun.updated_at,
+    username: workflowRun.triggering_actor?.login,
+    avatarUrl: workflowRun.triggering_actor?.avatar_url,
+    commitSubject: workflowRun.head_commit?.message,
+  };
+};
+
+export default handler;
