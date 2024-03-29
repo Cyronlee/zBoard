74726e75a263165a922546bd5492945e511ff342
Siyuan Li <siyuan.li2@thoughtworks.com>
2024-03-29 17:22:24 +0800

    feat: refine code

--- src/components/DashboardPreview.tsx
+++ src/components/DashboardPreview.tsx
@@ -1,73 +1,8 @@
 import { FC, ComponentProps, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { GridLayout } from '@/components/GridLayout';
-import ProjectTimeline from '@/components/ProjectTimeline';
-import ProjectTimelinePreview from '@/components/ProjectTimeline.preview.png';
-import TicketStatusOverview from '@/components/TicketStatusOverview';
-import TicketStatusOverviewPreview from '@/components/TicketStatusOverview.preview.png';
-import BuildStatusOverview from '@/components/BuildStatusOverview';
-import BuildStatusOverviewPreview from '@/components/BuildStatusOverview.preview.png';
-import OwnerRotationOverview from '@/components/OwnerRotationOverview';
-import OwnerRotationOverviewPreview from '@/components/OwnerRotationOverview.preview.png';
 import { usePageConfigStore } from '@/stores/pageConfig';
-
-const COMPONENTS = [
-  {
-    name: 'ProjectTimeline',
-    Component: (props: ComponentProps<typeof ProjectTimeline>) => (
-      <ProjectTimeline h="100%" {...props} />
-    ),
-    preview: ProjectTimelinePreview,
-    layout: {
-      w: 8,
-      minW: 2,
-      h: 7,
-      minH: 2,
-      maxH: 9,
-    },
-  },
-  {
-    name: 'TicketStatusOverview',
-    Component: (props: ComponentProps<typeof ProjectTimeline>) => (
-      <TicketStatusOverview h="100%" maxWidth="auto" {...props} />
-    ),
-    preview: TicketStatusOverviewPreview,
-    layout: {
-      w: 4,
-      minW: 2,
-      h: 6,
-      minH: 2,
-      maxH: 9,
-    },
-  },
-  {
-    name: 'BuildStatusOverview',
-    Component: (props: ComponentProps<typeof ProjectTimeline>) => (
-      <BuildStatusOverview h="100%" maxWidth="auto" {...props} />
-    ),
-    preview: BuildStatusOverviewPreview,
-    layout: {
-      w: 9,
-      minW: 2,
-      h: 6,
-      minH: 2,
-      maxH: 9,
-    },
-  },
-  {
-    name: 'OwnerRotationOverview',
-    Component: (props: ComponentProps<typeof ProjectTimeline>) => (
-      <OwnerRotationOverview h="100%" maxWidth="auto" {...props} />
-    ),
-    preview: OwnerRotationOverviewPreview,
-    layout: {
-      w: 2,
-      minW: 2,
-      h: 6,
-      minH: 4,
-    },
-  },
-];
+import { zBoardComponents } from '@/data/zBoardComponents';
 
 const Container = styled.div`
   width: 100vw;
@@ -93,7 +28,7 @@ const DashboardPreview: FC = () => {
       padding={padding}
       layouts={layouts}
       itemRender={({ component }) => {
-        const { Component } = COMPONENTS.find(({ name }) => name === component)!;
+        const { Component } = zBoardComponents.find(({ name }) => name === component)!;
         return <Component w="100%" h="100%" />;
       }}
     />
